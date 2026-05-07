2710b4f85cd267ab77d76d2e660f5d803d7495fb
Anders Lehn Reed <anders.lehn.reed@gmail.com>
2026-05-07 18:44:43 +0200

    prepare-release.py handles updating modules

--- prepare-release.py
+++ prepare-release.py
@@ -5,28 +5,41 @@
 # * the HTML pages
 # * the current modules interface for the Jython interpreter
 
+from dataclasses import dataclass
 import os
+from pathlib import Path
 from subprocess import call
 
+@dataclass
+class Commands:
+    mvn: str
+    npm: str
+
+def get_commands():
+    if os.name == "posix":
+        return Commands("mvn", "npm")
+    else:
+        return Commands("mvn.cmd", "npm.cmd")
+
+def update_modules(commands: Commands, current_dir: Path):
+    modules_path = current_dir.parent / "olca-modules"
+    app_path = current_dir / "olca-app"
+    print(f"install olca-modules from {modules_path}")
+    call(["mvn", "install", "-DskipTests"], cwd=modules_path)
+    print(f"update packages in {app_path}/libs")
+    call([commands.mvn, "-f", "pom_libs.xml", "package"], cwd=current_dir / "olca-app")
+    print("all done")
 
 def main():
-    if os.name == "posix":
-        call(["mvn", "-f", "pom_libs.xml", "clean"], cwd="./olca-app")
-        call("./update_modules.sh")
-        call(["mvn", "package"], cwd="./olca-refdata")
-        call(["npm", "install"], cwd="./olca-app-html")
-        call(["npm", "run", "build"], cwd="./olca-app-html")
-        call(["npm", "install"], cwd="./olca-app-build/credits")
-        call(["node", "credits-gen.js"], cwd="./olca-app-build/credits")
-    else:
-        call("mvn.cmd -f pom_libs.xml clean", cwd="./olca-app")
-        call("update_modules.bat")
-        call("mvn.cmd package", cwd="./olca-refdata")
-        call("npm.cmd install", cwd="./olca-app-html")
-        call("npm.cmd run build", cwd="./olca-app-html")
-        call("npm.cmd install", cwd="./olca-app-build/credits")
-        call("node credits-gen.js", cwd="./olca-app-build/credits")
-
+    commands = get_commands()
+    current_dir = Path(__file__).parent
+    call([commands.mvn, "-f", "pom_libs.xml", "clean"], cwd=current_dir / "olca-app")
+    update_modules(commands, current_dir)
+    call([commands.mvn, "package"], cwd=current_dir / "olca-refdata")
+    call([commands.npm, "install"], cwd=current_dir / "olca-app-html")
+    call([commands.npm, "run", "build"], cwd=current_dir / "olca-app-html")
+    call([commands.npm, "install"], cwd=current_dir / "olca-app-build/credits")
+    call(["node", "credits-gen.js"], cwd=current_dir / "olca-app-build/credits")
 
 if __name__ == "__main__":
     main()
